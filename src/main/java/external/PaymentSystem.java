bfa0e0914d3dde31c7c03361f975472b5031e550
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-04-20 15:49:33 +0300

    Create PaymentSystem.java

---
+++ src/main/java/external/PaymentSystem.java
@@ -0,0 +1,10 @@
+package external;
+
+
+public interface PaymentSystem
+{
+
+    boolean processPayment(String buyerAccountEmail, String sellerAccountEmail, double transactionAmount);
+
+    boolean processRefund(String buyerAccountEmail, String sellerAccountEmail, double transactionAmount);
+}
